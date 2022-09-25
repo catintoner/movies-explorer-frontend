10b2749a741d6b2ac4edb08108800c041a97c8a4
Dmitry Lukyanov <lukyanovd.a@yandex.ru>
2022-09-25 19:22:25 +0300

    feature: CRA

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Route, Switch, useHistory, withRouter } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='page'>
+
+    </div>
+  );
+}
+
+export default withRouter(App);
