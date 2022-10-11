fff49b8becf3032400d38e00a460fe5f950fecde
catintoner <lukyanov.dm.a@gmail.com>
2022-10-11 22:16:34 +0300

    feature: add burger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,10 +15,8 @@ function Header(props) {
   const [burgerChecked, setBurgerChecked] = React.useState(false);
 
   function handleBurgerCheck() {
-    setBurgerChecked(true);
+    setBurgerChecked(!burgerChecked);
   }
-
-
 
   React.useEffect(() => {
     console.log(windowSize);
@@ -49,12 +47,59 @@ function Header(props) {
         </li>
         {props.loggedIn ?
           (windowSize < 1150) ?
-            <>
-              <input className='header__burger-menu' id='burger-menu' type='checkbox'></input>
-              <label htmlFor='burger-menu'>
-
+            <div className='burger__menu'>
+              <input
+                className='header__burger-menu'
+                id='burger-menu' type='checkbox'
+                onClick={handleBurgerCheck}
+              ></input>
+              <label
+                htmlFor='burger-menu'
+                className={`header__burger-label ${burgerChecked && 'header__burger-label_type_open'}`}>
               </label>
-            </> :
+              <div className={`user__navigation ${burgerChecked && 'user__navigation_type_active'}`}>
+                <li className='nav__films'>
+                  <NavLink
+                    to={'/'}
+                    className={`header__link ${burgerChecked && 'user__navigation_type_active'}`}
+                  >
+                    Главная
+                  </NavLink>
+                </li>
+                <li className='nav__films'>
+                  <NavLink
+                    to={'/movies'}
+                    className={`header__link ${burgerChecked && 'user__navigation_type_active'}`}
+                    activeClassName='header__link_type_active-burger'
+                  >
+                    Фильмы
+                  </NavLink>
+                </li>
+
+                <li className={`nav__films ${burgerChecked && 'nav__films_type_last-child'}`}>
+                  <NavLink
+                    to={'/saved-movies'}
+                    className={`header__link ${burgerChecked && 'user__navigation_type_active'}`}
+                    activeClassName='header__link_type_active-burger'
+                  >
+                    Сохраненные фильмы
+                  </NavLink>
+                </li>
+
+                <li className='nav__profile'>
+                  <Link
+                    to={'/profile'}
+                    className='header__link header__link_type_profile'
+                  >
+                    <p className='profile__name'>
+                      Аккаунт
+                    </p>
+                    < div className='profile__logo'>
+                    </div>
+                  </Link>
+                </li>
+              </div>
+            </div> :
             <>
               <li className='nav__films'>
                 <NavLink
